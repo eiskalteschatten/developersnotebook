71d01563498fab4f813d7945ee704517af428b21
Alex <alex@alexseifert.com>
2018-04-07 01:45:58 +0200

    Cleaned up

--- src/windows/MainWindow.hpp
+++ src/windows/MainWindow.hpp
@@ -3,14 +3,6 @@
 
 #include <FL/Fl.H>
 #include <FL/Fl_Double_Window.H>
-#include <FL/Fl_Menu_Bar.H>
-// FOR MAC: #include <FL/Fl_Sys_Menu_Bar.H>
-// #include <FL/Fl_Window.H>
-// #include <FL/Fl_Input.H>
-// #include <FL/Fl_Button.H>
-// #include <FL/Fl_Return_Button.H>
-// #include <FL/Fl_Text_Editor.H>
-// #include <FL/Fl_Text_Buffer.H>
 
 class MainWindow: public Fl_Double_Window {
 private:
@@ -20,19 +12,6 @@ private:
 public:
     MainWindow(int width, int height, const char* title);
     virtual ~MainWindow() {};
-
-    Fl_Menu_Bar *menu_bar;
-
-    // Fl_Window        *replace_dlg;
-    // Fl_Input         *replace_find;
-    // Fl_Input         *replace_with;
-    // Fl_Button        *replace_all;
-    // Fl_Return_Button *replace_next;
-    // Fl_Button        *replace_cancel;
-    // Fl_Text_Editor   *editor;
-    // Fl_Text_Buffer   *textbuf;
-
-    // char search[256];
 };
 
 #endif /* MainWindow_hpp */
