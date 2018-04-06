7b3e1039226f86b7a73d88270a10e3bde463edd9
Alex Seifert <seifert@jochen-schweizer.de>
2018-04-06 17:24:20 +0200

    Cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,6 @@
 
 int main(int argc, char **argv) {
     MainWindow *window = new MainWindow(500, 300, "Develoepr's Notebook");
-
     window->show(argc, argv);
-
-    //if (argc > 1) load_file(argv[1], -1);
-
     return Fl::run();
 }
