8a1561761109263f5ec03f22f29ebcc6be1521bd
Alex <alex@alexseifert.com>
2018-04-07 00:03:10 +0200

    The main window is now resizable

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include "windows/MainWindow.hpp"
 
 int main(int argc, char **argv) {
-    MainWindow *window = new MainWindow(500, 300, "Developer's Notebook");
+    MainWindow *window = new MainWindow(1000, 700, "Developer's Notebook");
+    window->size_range(1, 1);
     window->show(argc, argv);
     return Fl::run();
 }
