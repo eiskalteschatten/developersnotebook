4334354e1ca057f8f2fbdecf2a33c6864f47ee2b
Alex <alex@alexseifert.com>
2018-04-07 21:58:20 +0200

    Fixed warning about gtk_vbox_new being deprecated

--- src/windows/MainWindow.cpp
+++ src/windows/MainWindow.cpp
@@ -31,15 +31,15 @@ void MainWindow::activate(GtkApplication *app, gpointer user_data) {
 }
 
 void MainWindow::setupMenuBar(GtkWidget *window) {
-    GtkWidget *vbox;
+    GtkWidget *box;
 
     GtkWidget *menubar;
     GtkWidget *fileMenu;
     GtkWidget *fileMi;
     GtkWidget *quitMi;
 
-    vbox = gtk_vbox_new(FALSE, 0);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), box);
 
     menubar = gtk_menu_bar_new();
     fileMenu = gtk_menu_new();
@@ -50,7 +50,7 @@ void MainWindow::setupMenuBar(GtkWidget *window) {
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi), fileMenu);
     gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quitMi);
     gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
-    gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), menubar, FALSE, FALSE, 0);
 
     // #ifdef __APPLE__
     //     gtk_widget_hide(menubar);
