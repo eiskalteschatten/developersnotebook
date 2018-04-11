a4d01e0092e0a71b8ca59413bf034b05a0972c03
Alex Seifert <seifert@jochen-schweizer.de>
2018-04-11 15:07:00 +0200

    Added resizable panes to the ProjectsView

--- src/views/ProjectsView.cpp
+++ src/views/ProjectsView.cpp
@@ -3,14 +3,25 @@
 #include "ProjectsView.hpp"
 
 ProjectsView::ProjectsView() {
-    main_widget = gtk_grid_new();
+    main_widget = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
     gtk_widget_set_halign(main_widget, GTK_ALIGN_FILL);
     gtk_widget_set_valign(main_widget, GTK_ALIGN_FILL);
 
-    gtk_grid_insert_row(GTK_GRID(main_widget), 0);
-    gtk_grid_insert_column(GTK_GRID(main_widget), 0);
+    GtkWidget *pv = gtk_label_new("projects list view");
+    GtkWidget *pv2 = gtk_label_new("projects form view");
 
-    GtkWidget *pv = gtk_label_new("projects view");
+    // Setup form grid
+    GtkWidget *form_grid = gtk_grid_new();
+    gtk_widget_set_halign(main_widget, GTK_ALIGN_FILL);
+    gtk_widget_set_valign(main_widget, GTK_ALIGN_FILL);
 
-    gtk_grid_attach(GTK_GRID(main_widget), pv, 0, 1, 1, 1);
+    gtk_grid_insert_row(GTK_GRID(form_grid), 0);
+    gtk_grid_insert_row(GTK_GRID(form_grid), 1);
+    gtk_grid_insert_column(GTK_GRID(form_grid), 1);
+
+    gtk_grid_attach(GTK_GRID(form_grid), pv2, 0, 1, 1, 1);
+
+    // Attach everything to the panes
+    gtk_paned_add1(GTK_PANED(main_widget), pv);
+    gtk_paned_pack2(GTK_PANED(main_widget), form_grid, FALSE, FALSE);
 }
