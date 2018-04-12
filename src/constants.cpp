4088f3086c73c33607b81ec2f39103b6489c1a77
Alex Seifert <seifert@jochen-schweizer.de>
2018-04-12 16:49:37 +0200

    Added global constants file

---
+++ src/constants.cpp
@@ -0,0 +1,8 @@
+// Global constants
+
+#if defined(__APPLE__) || defined(__linux__)
+    const char *PATH_TO_DB = "~/.devnotebook/developersnotebook.db";
+#else
+    const char *PATH_TO_DB = "windowspath";
+#endif
+
