fe35fe46fb118c819f9c32e9ea4f6e03e7a915e6
Alex <alex@alexseifert.com>
2018-04-13 20:40:14 +0200

    Continued setting up PreferencesModel

--- src/models/PreferencesModel.cpp
+++ src/models/PreferencesModel.cpp
@@ -9,20 +9,37 @@ static int create_callback(void *NotUsed, int argc, char **argv, char **azColNam
     return 0;
 }
 
-PreferencesModel::PreferencesModel() {
-    table_name = (char *)"preferences";
-    window_width = 1000;
-    window_height = 700;
+static int select_callback(void *data, int argc, char **argv, char **azColName) {
+    int i;
+    fprintf(stderr, "%s: ", (const char*)data);
 
-    initialize();
+    for(i = 0; i<argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+    return 0;
 }
 
-PreferencesModel::PreferencesModel(int &select_id) {
+PreferencesModel::PreferencesModel() {
+    char *error_message = 0;
+    int connection;
+
+    id = 1; // id is 1 because there is only ever 1 row
     table_name = (char *)"preferences";
     window_width = 1000;
     window_height = 700;
 
     initialize();
+
+    std::string sql = "SELECT * FROM " + (std::string)table_name + " WHERE ID=" + std::to_string(id) + ";";
+
+    connection = sqlite3_exec(connection_manager->get_db(), sql.c_str(), select_callback, 0, &error_message);
+
+    if(connection != SQLITE_OK){
+        fprintf(stderr, "SQL error: %s\n", error_message);
+        sqlite3_free(error_message);
+    }
 }
 
 PreferencesModel::~PreferencesModel() {
