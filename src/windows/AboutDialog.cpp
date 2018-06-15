32f9dfa68bdc0f6d9f9c4e514232b1cb6d616e57
Alex <alex@alexseifert.com>
2018-06-15 20:01:03 +0200

    Started trying to add the application icon to the about dialog

--- src/windows/AboutDialog.cpp
+++ src/windows/AboutDialog.cpp
@@ -1,20 +1,25 @@
 #include <string>
+#include <unistd.h>
 #include <gtk/gtk.h>
 
 #include "AboutDialog.hpp"
 #include "../constants.hpp"
 
-GtkWidget *dialog;
-
-const std::string copyright = "(c) Alex Seifert 2018";
-const std::string comments = "A digital notebook for developers";
-const std::string website = "https://www.alexseifert.com";
-
 
 void AboutDialog::activate() {
-    //GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file("../resources/templogo2.jpg", NULL);
+    const std::string copyright = "(c) Alex Seifert 2018";
+    const std::string comments  = "A digital notebook for developers";
+    const std::string website   = "https://www.alexseifert.com";
 
-    dialog = gtk_about_dialog_new();
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+
+    const char *icon_path = strcat(cwd, "/resources/images/logo.png");
+
+    GError *error;
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(icon_path, &error);
+
+    GtkWidget *dialog = gtk_about_dialog_new();
 
     gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER);
 
@@ -24,9 +29,9 @@ void AboutDialog::activate() {
     gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog), comments.c_str());
     gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), website.c_str());
 
-    // gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dialog), pixbuf);
-    // g_object_unref(pixbuf), pixbuf = NULL;
+    gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dialog), pixbuf);
+    g_object_unref(pixbuf);
 
-    gtk_dialog_run(GTK_DIALOG (dialog));
+    gtk_dialog_run(GTK_DIALOG(dialog));
     gtk_widget_destroy(dialog);
 }
