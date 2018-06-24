15cbb2b7cc29a222f438a20f5b8e92e6d5c73e2c
Alex <alex@alexseifert.com>
2018-06-24 17:15:35 +0200

    Started adding the release notes to the dashboard

--- src/views/DashboardView.cpp
+++ src/views/DashboardView.cpp
@@ -15,58 +15,93 @@ DashboardView::DashboardView() {
     gtk_container_set_border_width(GTK_CONTAINER(main_widget), grid_spacing);
 
     gtk_grid_insert_row(GTK_GRID(main_widget), 0);
+    gtk_grid_insert_row(GTK_GRID(main_widget), 1);
     gtk_grid_insert_column(GTK_GRID(main_widget), 0);
 
-    // Application logo, title, etc
-    GtkWidget *title_grid = gtk_grid_new();
+    // Setup grid for application title, logo, etc
+    {
+        GtkWidget *title_grid = gtk_grid_new();
 
-    gtk_grid_insert_row(GTK_GRID(title_grid), 0);
-    gtk_grid_insert_row(GTK_GRID(title_grid), 1);
-    gtk_grid_insert_row(GTK_GRID(title_grid), 2);
-    gtk_grid_insert_column(GTK_GRID(title_grid), 0);
-    gtk_grid_insert_column(GTK_GRID(title_grid), 1);
+        gtk_grid_insert_row(GTK_GRID(title_grid), 0);
+        gtk_grid_insert_row(GTK_GRID(title_grid), 1);
+        gtk_grid_insert_row(GTK_GRID(title_grid), 2);
+        gtk_grid_insert_column(GTK_GRID(title_grid), 0);
+        gtk_grid_insert_column(GTK_GRID(title_grid), 1);
 
-    // -- Logo
-    Image logo_image("icon128x128", "svg");
-    GdkPixbuf *logo_buf = logo_image.get_pixbuf();
-    GtkWidget *logo     = gtk_image_new_from_pixbuf(logo_buf);
-    g_object_unref(logo_buf);
-    gtk_grid_attach(GTK_GRID(title_grid), logo, 0, 0, 1, 3);
+        // -- Logo
+        Image logo_image("icon128x128", "svg");
+        GdkPixbuf *logo_buf = logo_image.get_pixbuf();
+        GtkWidget *logo     = gtk_image_new_from_pixbuf(logo_buf);
+        g_object_unref(logo_buf);
+        gtk_grid_attach(GTK_GRID(title_grid), logo, 0, 0, 1, 3);
 
-    gchar *markup = nullptr;
+        gchar *markup = nullptr;
 
-    // -- Title
-    GtkWidget *title          = gtk_label_new(NULL);
-    const gchar *app_name     = Constants::application_name.c_str();
-    const gchar *format_title = "<span font_weight=\"heavy\" font_size=\"xx-large\">\%s</span>";
-    markup                    = g_markup_printf_escaped(format_title, app_name);
-    gtk_label_set_markup(GTK_LABEL(title), markup);
+        // Title
+        GtkWidget *title          = gtk_label_new(NULL);
+        const gchar *app_name     = Constants::application_name.c_str();
+        const gchar *format_title = "<span font_weight=\"heavy\" font_size=\"xx-large\">\%s</span>";
+        markup                    = g_markup_printf_escaped(format_title, app_name);
+        gtk_label_set_markup(GTK_LABEL(title), markup);
 
-    g_object_set(title, "hexpand", TRUE, NULL);
-    gtk_widget_set_halign(title, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(title_grid), title, 1, 0, 1, 1);
+        g_object_set(title, "hexpand", TRUE, NULL);
+        gtk_widget_set_halign(title, GTK_ALIGN_START);
+        gtk_grid_attach(GTK_GRID(title_grid), title, 1, 0, 1, 1);
 
-    // -- Slogan
-    GtkWidget *slogan          = gtk_label_new(NULL);
-    const gchar *app_slogan    = Constants::application_slogan.c_str();
-    const gchar *format_slogan = "<span font_size=\"large\">\%s</span>";
-    markup                     = g_markup_printf_escaped(format_slogan, app_slogan);
-    gtk_label_set_markup(GTK_LABEL(slogan), markup);
+        // Slogan
+        GtkWidget *slogan          = gtk_label_new(NULL);
+        const gchar *app_slogan    = Constants::application_slogan.c_str();
+        const gchar *format_slogan = "<span font_size=\"large\">\%s</span>";
+        markup                     = g_markup_printf_escaped(format_slogan, app_slogan);
+        gtk_label_set_markup(GTK_LABEL(slogan), markup);
 
-    g_object_set(slogan, "hexpand", TRUE, NULL);
-    gtk_widget_set_halign(slogan, GTK_ALIGN_START);
-    gtk_widget_set_valign(slogan, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(title_grid), slogan, 1, 1, 1, 1);
+        g_object_set(slogan, "hexpand", TRUE, NULL);
+        gtk_widget_set_halign(slogan, GTK_ALIGN_START);
+        gtk_widget_set_valign(slogan, GTK_ALIGN_START);
+        gtk_grid_attach(GTK_GRID(title_grid), slogan, 1, 1, 1, 1);
 
-    // -- Website
-    GtkWidget *website = gtk_link_button_new(Constants::application_website.c_str());
-    g_object_set(website, "hexpand", TRUE, NULL);
-    gtk_widget_set_halign(website, GTK_ALIGN_START);
-    gtk_widget_set_valign(website, GTK_ALIGN_START);
-    gtk_grid_attach(GTK_GRID(title_grid), website, 1, 2, 1, 1);
+        // Website
+        GtkWidget *website = gtk_link_button_new(Constants::application_website.c_str());
+        g_object_set(website, "hexpand", TRUE, NULL);
+        gtk_widget_set_halign(website, GTK_ALIGN_START);
+        gtk_widget_set_valign(website, GTK_ALIGN_START);
+        gtk_grid_attach(GTK_GRID(title_grid), website, 1, 2, 1, 1);
 
+        g_free(markup);
 
-    g_free(markup);
+        gtk_grid_attach(GTK_GRID(main_widget), title_grid, 0, 0, 1, 1);
+    }
 
-    gtk_grid_attach(GTK_GRID(main_widget), title_grid, 0, 0, 1, 1);
+    // Release Notes
+    {
+        GtkWidget *release_notes_grid = gtk_grid_new();
+        gtk_grid_set_row_spacing(GTK_GRID(release_notes_grid), 10);
+
+        gtk_grid_insert_row(GTK_GRID(release_notes_grid), 0);
+        gtk_grid_insert_row(GTK_GRID(release_notes_grid), 1);
+        gtk_grid_insert_column(GTK_GRID(release_notes_grid), 0);
+
+        // Title
+        GtkWidget *title          = gtk_label_new(NULL);
+        const gchar *format_title = "<span font_weight=\"heavy\" font_size=\"xx-large\">\%s</span>";
+        const gchar *markup       = g_markup_printf_escaped(format_title, "Release Notes");
+        gtk_label_set_markup(GTK_LABEL(title), markup);
+
+        g_object_set(title, "hexpand", TRUE, NULL);
+        gtk_widget_set_halign(title, GTK_ALIGN_START);
+        gtk_grid_attach(GTK_GRID(release_notes_grid), title, 0, 0, 1, 1);
+
+        // Release Notes Text View
+        GtkWidget *release_notes_text_view = gtk_text_view_new();
+        gtk_text_view_set_editable(GTK_TEXT_VIEW(release_notes_text_view), false);
+
+        GtkWidget *release_notes_scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+        g_object_set(release_notes_scrolled_window, "hexpand", TRUE, NULL);
+        g_object_set(release_notes_scrolled_window, "vexpand", TRUE, NULL);
+        gtk_container_add(GTK_CONTAINER(release_notes_scrolled_window), release_notes_text_view);
+
+        gtk_grid_attach(GTK_GRID(release_notes_grid), release_notes_scrolled_window, 0, 1, 1, 1);
+
+        gtk_grid_attach(GTK_GRID(main_widget), release_notes_grid, 0, 1, 1, 1);
+    }
 }
