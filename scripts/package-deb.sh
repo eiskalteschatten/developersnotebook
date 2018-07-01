280532c7fd5a80b26625bec8dcadd870a3fa9d93
Alex Seifert <alex@alexseifert.com>
2018-07-01 10:04:37 +0100

    Create a desktop menu entry when installing the .deb package

--- scripts/package-deb.sh
+++ scripts/package-deb.sh
@@ -16,11 +16,13 @@ cp -R ./pkg/DEBIAN ${TARGET_DIR}/.
 cp ./${BIN_DIR}/${TARGET} ${DEVNOTEBOOK_DIR}/.
 cp ./LICENSE ${DEVNOTEBOOK_DIR}/.
 cp ./README.md ${DEVNOTEBOOK_DIR}/.
+cp ./pkg/developersnotebook.desktop ${DEVNOTEBOOK_DIR}/.
 cp -R ./resources/ ${DEVNOTEBOOK_DIR}/.
 
 rm -rf ${DEVNOTEBOOK_DIR}/resources/macos
 
 chmod +x ${DEVNOTEBOOK_DIR}/${TARGET}
+chmod +x ${TARGET_DIR}/DEBIAN/postinst
 
 cd ./${BIN_DIR}
 
