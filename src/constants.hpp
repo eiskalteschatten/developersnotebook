8095f6b88dcc57d5bd0a5efbb423544ba53e915f
Alex Seifert <alex@alexseifert.com>
2018-07-01 10:28:27 +0100

    Use Boost to get the path to the resources

--- src/constants.hpp
+++ src/constants.hpp
@@ -18,6 +18,7 @@ namespace Constants {
     const std::string path_to_db = path_to_config + "developersnotebook.db";
 
     const std::string application_name     = "Developer's Notebook";
+    const std::string application_exec     = "developersnotebook";
     const std::string application_version  = "0.1";
     const std::string application_slogan   = "A digital notebook for developers";
     const std::string application_website  = "https://www.alexseifert.com";
--- src/util/Image.cpp
+++ src/util/Image.cpp
@@ -1,3 +1,4 @@
+#include <boost/filesystem.hpp>
 #include <unistd.h>
 #include <fstream>
 #include <cstring>
@@ -9,6 +10,8 @@
 
 #include "Image.hpp"
 #include "../constants.hpp"
+
+namespace fs = boost::filesystem;
 
 
 Image::Image(const std::string new_name, const std::string new_extension) {
@@ -28,9 +31,22 @@ Image::~Image() {
 
 std::string Image::get_icon_path() {
     char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    const std::string path = Constants::path_to_resources + "/images/" + name + "." + extension;
-    return std::string(std::strcat(cwd, path.c_str()));
+    readlink("/proc/self/exe", &cwd[0], sizeof(cwd));
+
+    fs::path path_to_exec = std::string(cwd);
+    path_to_exec.remove_filename();
+    path_to_exec /= Constants::path_to_resources;
+
+    if (!fs::is_directory(path_to_exec)) {
+        path_to_exec.remove_leaf();
+        path_to_exec.remove_leaf();
+    }
+
+    path_to_exec /= Constants::path_to_resources;
+    path_to_exec /= "images";
+    path_to_exec /= name + "." + extension;
+
+    return path_to_exec.string();
 }
 
 std::string Image::get_icon_path_mac() {
